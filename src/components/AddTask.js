72446318097e347e31a73a6679f6c31a3305e30c
Nathaniel C Napier <nattydevs@gmail.com>
2022-09-07 15:46:44 -0500

    Add files via upload

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -6,6 +6,9 @@ let user = "name"
 const AddTask = ({onAdd}) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
+    const [day1, setDay1] = useState('')
+    const [plate1, setPlate1] = useState('')
+    const [plate2, setPlate2] = useState('')
     const [reminder, setReminder] = useState(false)
     const [location, setLocation] = useState('')
     const onSubmit = (e) => {
@@ -13,10 +16,13 @@ const AddTask = ({onAdd}) => {
 
        
 
-        onAdd({text, day, reminder, location})
+        onAdd({text, day, day1, plate1, plate2, reminder, location})
 
         setText('')
+        setPlate1('')
+        setPlate2('')
         setDay(Date)
+        setDay1(Date)
         setReminder(false)
         setLocation('')
     }
@@ -25,14 +31,17 @@ const AddTask = ({onAdd}) => {
     <form className='add-form' onSubmit={onSubmit}>
         <div className='form-control'>
             <label style={{color: Lion.defaultProps.color3}}>User</label>
-            <select>
-            <option value={text}>Nathaniel</option>
-            <option value={text}>Olivier</option>
-            <option value={text}>Katherine</option>
-            <option value={text}>Sichen</option>
-            <option value={text}>Chandra</option>
-            <option value={text}>Irene</option>
+            
+            <select value={text} onChange={(e) => setText(e.target.value)}>
+            <option value={''}>Name</option>
+            <option value={'Nathaniel'}>Nathaniel</option>
+            <option value={'Olivier'}>Olivier</option>
+            <option value={'Katherine'}>Katherine</option>
+            <option value={'Sichen'}>Sichen</option>
+            <option value={'Chandra'}>Chandra</option>
+            <option value={'Irene'}>Irene</option>
             </select>
+            
             
             
 
@@ -44,9 +53,36 @@ const AddTask = ({onAdd}) => {
         </div>
         <div className='form-control'>
             <label style={{color: Lion.defaultProps.color3}}>Run End Day & Time</label>
-            <input type='datetime-local' placeholder='Run End Date & Time'value={day} onChange={(e) => setDay(e.target.value)}></input>
+            <input type='datetime-local' placeholder='Run End Date & Time'value={day1} onChange={(e) => setDay1(e.target.value)}></input>
         </div>
-        
+        <div className='form-control plates'>
+            <label style={{color: Lion.defaultProps.color3}}>Plate 1:</label>
+            
+            <select value={plate1} onChange={(e) => setPlate1(e.target.value)}>
+            <option value={''}>none</option>
+            <option value={'Position 1'}>Position 1</option>
+            <option value={'Position 2'}>Position 2</option>
+            <option value={'Position 3'}>Position 3</option>
+            <option value={'Position 4'}>Position 4</option>
+            <option value={'Position 5'}>Position 5</option>
+            <option value={'Position 6'}>Position 6</option>
+            </select>
+            
+            <label style={{color: Lion.defaultProps.color3}}>Plate 2:</label>
+            
+            <select value={plate2} onChange={(e) => setPlate2(e.target.value)}>
+            <option value={''}>none</option>
+            <option value={'Position 1'}>Position 1</option>
+            <option value={'Position 2'}>Position 2</option>
+            <option value={'Position 3'}>Position 3</option>
+            <option value={'Position 4'}>Position 4</option>
+            <option value={'Position 5'}>Position 5</option>
+            <option value={'Position 6'}>Position 6</option>
+            </select>
+            
+
+            
+        </div>
         <div className='form-control form-control-check'>
             <label style={{color: Lion.defaultProps.color3}}>Leave Plates</label>
             <input type='checkbox' checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}></input>
--- src/components/Task.js
+++ src/components/Task.js
@@ -11,11 +11,20 @@ const Task = ({task, onDelete, onToggle}) => {
   const year = task.day.split('').splice(0,4).join('')
   const day = task.day.split('').splice(8,2).join('')
   const time = task.day.split('').splice(11,5).join('')
+
+  const month1 = task.day1.split('').splice(5,2).join('') 
+  const year1 = task.day1.split('').splice(0,4).join('')
+  const day1 = task.day1.split('').splice(8,2).join('')
+  const time1 = task.day1.split('').splice(11,5).join('')
+  console.log(task)
+  const text = task.text
+  const plate1 = task.plate1
+  const plate2 = task.plate2
   return (
     <div style={{backgroundColor: Lion.defaultProps.color2, transparency: 20}}className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
-        <h3 style={{color: "white"}}>{task.reminder ? `${task.text} @ ${Lion.defaultProps.team}` : `${Lion.defaultProps.team} @ ${task.text}`} <FaTimes style={{color: 'red', cursor: 'pointer'}} 
+        <h3 style={{color: "white"}}> { text + " is running plates in: " }<br></br>{ plate1 + " and " + plate2 + " from: "  } <FaTimes style={{color: 'rgb(32, 122, 146)', cursor: 'pointer'}} 
         onClick={() => onDelete(task.id)}/></h3>
-        <p style={{color: "white"}}>{month + '/'+ day +'/'+ year + ' @ ' + time}</p>
+        <p style={{color: "white"}}>{month + '/'+ day +'/'+ year +'(' + time + ')  ' + '- '+ month1 + '/'+ day1 +'/'+ year1 +'(' + time1 + ')'}</p>
         <p style={{color: "white"}}>{task.location}</p>
     </div>
   )
